c96c8401473f3b1061f2c031e1651ac5fb97dec0
yoonemong <yoonemong@naver.com>
2020-02-01 15:33:37 +0900

    BOJ 16929 solved

--- src/graph/Main16929.java
+++ src/graph/Main16929.java
@@ -3,17 +3,51 @@ package graph;
 import java.util.Scanner;
 
 public class Main16929 {
+	private static char[][] a;
+	private static boolean[][] check;
+	private static int n,m;
+	final static int[] dx = {0,0,1,-1};
+	final static int[] dy = {1,-1,0,0};
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		int[][] map = new int[N][M];
-		for(int i=0;i<N;i++) {
-			String temp = sc.nextLine();
-			for(int j=0;j<M;j++) {
-				map[i][j] = temp.charAt(j) - 'A';
+	static boolean go(int x,int y,int px,int py,char color) {
+		if(check[x][y]) {
+			return true;
+		}
+		check[x][y] = true;
+		
+		for(int i=0;i<4;i++) {
+			int tx = x + dx[i];
+			int ty = y + dy[i];
+			
+			if(0<=tx && tx<n && 0<=ty && ty<m) {
+				if(!(tx==px && ty == py) && a[tx][ty] == color) {
+					return go(tx,ty,x,y,color);
+				}
 			}
 		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		a = new char[n][m];
+		check = new boolean[n][m];
+		for(int i=0;i<n;i++) {
+			a[i] = sc.next().toCharArray();
+		}
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				if(check[i][j]) {
+					continue;
+				}
+				if(go(i,j,-1,-1,a[i][j])) {
+					System.out.println("Yes");
+					System.exit(0);
+				}
+			}
+		}
+		System.out.println("No");
 	}
 }
